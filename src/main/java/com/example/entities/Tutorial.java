d7b577d90b34ace23b3f83b5732f2650cea61ea1
OlegPetrov <antey1988@gmail.com>
2021-06-09 17:23:10 +0300

    Tutorials: create, update, delete and list

---
+++ src/main/java/com/example/entities/Tutorial.java
@@ -0,0 +1,61 @@
+package com.example.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TUTORIAL")
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "published")
+    private Boolean published;
+
+    public Tutorial() {
+    }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+}
